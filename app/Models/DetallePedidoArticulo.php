ea08c3b9fff0b17e8834b419cb8e9054ff98d45c
oswandor26 <oswandor26@gmailc.om>
2023-05-10 13:58:45 -0600

    Models Detallepedidos

--- app/Models/DetallePedidoArticulo.php
+++ app/Models/DetallePedidoArticulo.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class DetallePedidoArticulo extends Model
 {
     use HasFactory;
+    protected $table = 'tb_detalle_pedido_articulos';
+    protected $fillable = [
+        'fk_id_articulo',
+        'fk_id_pedido',
+        'cantidad_detalle',
+        'descuento_detalle'
+    ];
+    public $timestamps = false;
+
+    public function articulo()
+    {
+        return $this->belongsTo('App\Articulo', 'fk_id_articulo', 'id_articulo');
+    }
+
+    public function pedido()
+    {
+        return $this->belongsTo('App\Pedido', 'fk_id_pedido', 'id_pedido');
+    }
 }
