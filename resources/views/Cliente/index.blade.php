50adae366e7fe93586b5eb09229aafdd8413746c
oswandor26 <oswandor26@gmailc.om>
2023-05-11 22:22:27 -0600

    list Clients

---
+++ resources/views/Cliente/index.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>List Of Clients</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+</head>
+<body>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2>List Clients</h2>
+                </div>
+                <div class="pull-right mb-2">
+                 <a class="btn btn-success" href="{{ route('clientes.create') }}"> Create Client</a>
+                </div>
+            </div>
+        </div>
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
+        @endif
+        <table class="table table-bordered">
+        <thead>
+    <tr>
+        <th>S.No</th>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>Fecha Nacimiento</th>
+    </tr>
+</thead>
+<tbody>
+    @foreach ($clientes as $cliente)
+    <tr>
+        <td>{{ $cliente->id_cliente }}</td>
+        <td>{{ $cliente->nombre_cliente }}</td>
+        <td>{{ $cliente->apellido_cliente }}</td>
+        <td>{{ $cliente->fecha_nacimiento_cliente }}</td>
+        <td>
+        
+        </td>
+    </tr>
+    @endforeach
+</tbody>
+
+        </table>
+      
+    </div>
+</body>
+</html>
